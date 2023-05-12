3b59d906f75e0b3e358872a5ec248334363e9e6b
harish-madineni-kr <harish.madineni@kroger.com>
2023-05-12 11:34:59 +0530

    Added a fully working example.

--- file_upload_app/file_app/urls.py
+++ file_upload_app/file_app/urls.py
@@ -4,11 +4,12 @@ from . import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('admin_login/', views.admin_login, name='admin_login'),
-    path('admin_panel/', views.admin_panel, name='admin_panel'),
-    path('upload/', views.upload, name='upload'),
-    path('download/', views.download, name='download'),
-    path('download_file/<str:file>/', views.download_file, name='download_file'),
-    path('open_file/<str:file>/', views.open_file, name='open_file'),
+    path("", views.home, name="home"),
+    path("admin_login/", views.admin_login, name="admin_login"),
+    path("admin_panel/", views.admin_panel, name="admin_panel"),
+    path("upload/", views.upload, name="upload"),
+    # path("download/", views.download, name="download"),
+    path("download_file/<str:file>/", views.download_file, name="download_file"),
+    # path("open/", views.open, name="open"),
+    path("open_file/<str:file>/", views.open_file, name="open_file"),
 ]
--- file_upload_app/file_app/views.py
+++ file_upload_app/file_app/views.py
@@ -6,101 +6,134 @@ from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='/admin/')
+
+@login_required(login_url="/admin/")
 def home(request):
     try:
-        if request.method == 'POST' and request.FILES['file']:
-            myfile = request.FILES['file']
+        if request.method == "POST" and request.FILES["file"]:
+            myfile = request.FILES["file"]
             fs = FileSystemStorage()
             fs.save(myfile.name, myfile)
-            return redirect('admin_panel')
+            return redirect("admin_panel")
     except Exception as e:
-        exception = {"exception" : e, "messages" : "Upload failed with exception."}
-        return render(request, 'error.html', exception)
-    
-    return render(request, 'home.html')
+        exception = {"exception": e, "messages": "Upload failed with exception."}
+        print("Execption printing: ", e)
+        return render(request, "error.html", exception)
 
-@login_required(login_url='/admin/')
+    return render(request, "home.html")
+
+
+@login_required(login_url="/admin/")
 def admin_login(request):
     try:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            if username == 'admin' and password == 'admin':
-                return redirect('admin_panel')
+        if request.method == "POST":
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            if username == "admin" and password == "admin":
+                return redirect("admin_panel")
             else:
-                return render(request, 'admin_login.html', {'error': 'Invalid Credentials'})
+                return render(
+                    request, "admin_login.html", {"error": "Invalid Credentials"}
+                )
     except Exception as e:
-        exception = {"exception" : e, "messages" : "Upload failed with exception."}
-        return render(request, 'error.html', exception)
-    
-    return render(request, 'admin_login.html')
+        exception = {"exception": e, "messages": "Upload failed with exception."}
+        print("Execption printing: ", e)
+        return render(request, "error.html", exception)
 
-@login_required(login_url='/admin/')
+    return render(request, "admin_login.html")
+
+
+@login_required(login_url="/admin/")
 def admin_panel(request):
     files = []
     try:
+        # file_name = request.file
         temp = os.listdir(settings.MEDIA_ROOT)
         print("list dir exec")
         for file_name in temp:
-            if (file_name.endswith('.csv') or file_name.endswith('.xlsx')) and file_name != '':
+            if (
+                file_name.endswith(".csv") or file_name.endswith(".xlsx")
+            ) and file_name != "":
                 file_path = os.path.join(settings.MEDIA_ROOT, file_name)
-                files.append((file_name, os.path.getsize(file_path)))
+                files.append(
+                    {"file_name": file_name, "size": os.path.getsize(file_path)}
+                )
     except Exception as e:
-        exception = {"exception" : e, "messages" : "Upload failed with exception."}
-        print(e)
-        return render(request, 'error.html', exception)
+        exception = {"exception": e, "messages": "Upload failed with exception."}
+        print("Execption printing: ", e)
+        return render(request, "error.html", exception)
 
-    return render(request, 'admin.html', {'files': files})
+    return render(request, "admin.html", {"files": files})
 
-@login_required(login_url='/admin/')
-def download(request):
-    download_file(request, "test.csv")
-    pass
 
-@login_required(login_url='/admin/')
+# @login_required(login_url="/admin/")
+# def download(request):
+#     return download_file(request, "test.csv")
+
+
+@login_required(login_url="/admin/")
 def download_file(request, file):
-    file_path = os.path.join(settings.MEDIA_ROOT, file)
     try:
+        file_path = os.path.join(settings.MEDIA_ROOT, file)
         if os.path.exists(file_path):
-            with open(file_path, 'rb') as fh:
-                response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
-                response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            with open(file_path, "rb") as fh:
+                response = HttpResponse(
+                    fh.read(), content_type="application/vnd.ms-excel"
+                )
+                response[
+                    "Content-Disposition"
+                ] = "inline; filename=" + os.path.basename(file_path)
                 return response
     except Exception as e:
-        exception = {"exception" : e, "messages" : "Upload failed with exception."}
-        return render(request, 'error.html', exception)
+        exception = {"exception": e, "messages": "Upload failed with exception."}
+        print("Execption printing: ", e)
+        return render(request, "error.html", exception)
     raise Http404
 
-@login_required(login_url='/admin/')
-def open_file(request, file_name):
+
+# @login_required(login_url="/admin/")
+# def open(request):
+#     return open_file(request, "test.csv")
+
+
+@login_required(login_url="/admin/")
+def open_file(request, file, *args):
     try:
-        file_path = os.path.join(settings.MEDIA_ROOT, file_name)
+        print("file object: ", file)
+        file_path = os.path.join(settings.MEDIA_ROOT, file)
         if os.path.exists(file_path):
-            if file_name.endswith('.csv'):
+            if file.endswith(".csv"):
+                # print("file path: ", file_path)
                 df = pd.read_csv(file_path)
-            elif file_name.endswith('.xlsx'):
+            elif file.endswith(".xlsx"):
                 df = pd.read_excel(file_path)
             else:
-                return HttpResponse('Invalid File Type')
-            return render(request, 'open_file.html', {'file_name': file_name, 'table_html': df.to_html(index=False)})
+                return HttpResponse("Invalid File Type")
+            return render(
+                request,
+                "open_file.html",
+                {"file": file, "table_html": df.to_html(index=True)},
+            )
     except Exception as e:
-        exception = {"exception" : e, "messages" : "Upload failed with exception."}
-        return render(request, 'upload.html', exception)
+        exception = {"exception": e, "messages": ["Upload failed with exception."]}
+        print("Execption printing: ", e)
+        return render(request, "error.html", exception)
     raise Http404
 
-@login_required(login_url='/admin/')
+
+@login_required(login_url="/admin/")
 def upload(request):
     try:
-        if request.method == 'POST' and request.FILES['file']:
-            myfile = request.FILES['file']
+        if request.method == "POST" and request.FILES["file"]:
+            myfile = request.FILES["file"]
             fs = FileSystemStorage(location=settings.MEDIA_ROOT)
             filename = fs.save(myfile.name, myfile)
             uploaded_file_url = fs.url(filename)
-            return render(request, 'upload.html', {
-                'uploaded_file_url': uploaded_file_url
-            })
+            return render(
+                request, "upload.html", {"uploaded_file_url": uploaded_file_url}
+            )
     except Exception as e:
-        exception = {"exception" : e, "messages" : "Upload failed with exception."}
-        return render(request, 'error.html', exception)
-    return render(request, 'upload.html')
+        exception = {"exception": e, "messages": "Upload failed with exception."}
+        print("Execption printing: ", e)
+        return render(request, "error.html", exception)
+    return render(request, "upload.html")
